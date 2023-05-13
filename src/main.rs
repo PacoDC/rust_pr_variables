0fa57b0016125fc242f903194d7d4dcb34064d1c
PacoDC <pacodiazcatalan@live.com>
2023-05-13 18:15:57 +0200

    Ejemplo con variables de varios tipos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let un_numero: i64 = 55;
+    let otro_numero = 23;
+    let otro2_num: u8 = 255;
+    let otro3_num: i16 = 30;
+
+    println!("Prueba de variables: ");
+    println!("Primer número: {}", un_numero);
+    println!("Segundo número {}", otro_numero);
+    println!("Tercer número {}", otro2_num);
+    println!("Cuarto número {}", otro3_num);
+    
 }
